52fca877574e8612f153392d3551f68eef0ea688
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-08-01 22:12:14 -0300

    Update ImplementaçãoGrafos.cpp

--- ImplementaçãoGrafos.cpp
+++ ImplementaçãoGrafos.cpp
@@ -221,145 +221,6 @@ void consultarListaAdjacentes(int vertice)
     }
 }
 
-void visitaEmProfundidade(int vertice)
-{
-    cor[vertice] = 1;
-    tempo++;
-    descoberta[vertice] = tempo;
-    cout << "Descoberta do vértice " << vertice << " no tempo " << tempo << "\n";
-
-    for (int i = 0; i < adjacencias[vertice].size(); i++)
-    {
-        int v = adjacencias[vertice][i].vertice;
-        if (cor[v] == 0)
-        {
-            antecessor[v] = vertice;
-            visitaEmProfundidade(v);
-        }
-    }
-
-    cor[vertice] = 2;
-    tempo++;
-    termino[vertice] = tempo;
-    cout << "Finalização do vértice " << vertice << " no tempo " << tempo << "\n";
-}
-
-void buscaEmProfundidade()
-{
-    descoberta.assign(numVertices, 0);
-    termino.assign(numVertices, 0);
-    antecessor.assign(numVertices, -1);
-    cor.assign(numVertices, 0);
-    tempo = 0;
-
-    for (int i = 0; i < numVertices; i++)
-    {
-        if (cor[i] == 0)
-        {
-            visitaEmProfundidade(i);
-        }
-    }
-}
-
-void visitaEmLargura(int vertice)
-{
-    vector<int> fila;
-    cor[vertice] = 1;
-    distancia[vertice] = 0;
-    fila.push_back(vertice);
-    cout << "Descoberta do vértice " << vertice << "\n";
-
-    while (!fila.empty())
-    {
-        int v = fila.front();
-        fila.erase(fila.begin());
-
-        for (int i = 0; i < adjacencias[v].size(); i++)
-        {
-            int k = adjacencias[v][i].vertice;
-            if (cor[k] == 0)
-            {
-                cor[k] = 1;
-                antecessor[k] = v;
-                distancia[k] = distancia[v] + 1;
-                fila.push_back(k);
-                cout << "Descoberta do vértice " << k << " com distância " << distancia[k] << "\n";
-            }
-        }
-        cor[v] = 2;
-        cout << "Finalização do vértice " << v << "\n";
-    }
-}
-
-void buscaEmLargura()
-{
-    antecessor.assign(numVertices, -1);
-    cor.assign(numVertices, 0);
-    distancia.assign(numVertices, 1000);
-
-    for (int i = 0; i < numVertices; i++)
-    {
-        if (cor[i] == 0)
-        {
-            visitaEmLargura(i);
-        }
-    }
-}
-
-void prim()
-{
-    vector<int> menorPesoAresta(numVertices, 1000);
-    vector<int> verticePai(numVertices, -1);
-    vector<bool> estaNoCirculo(numVertices, false);
-
-    menorPesoAresta[0] = 0;
-
-    for (int i = 0; i < numVertices - 1; i++)
-    {
-        int minValor = 1000, u = -1;
-
-        for (int v = 0; v < numVertices; v++)
-        {
-            if (!estaNoCirculo[v] && menorPesoAresta[v] < minValor)
-            {
-                minValor = menorPesoAresta[v];
-                u = v;
-            }
-        }
-
-        if (u == -1)
-            break;
-
-        estaNoCirculo[u] = true;
-        cout << "Vértice " << u << " adicionado a circulo.\n";
-
-        for (int j = 0; j < adjacencias[u].size(); j++)
-        {
-            int v = adjacencias[u][j].vertice;
-            int peso = adjacencias[u][j].peso;
-
-            if (!estaNoCirculo[v] && peso < menorPesoAresta[v])
-            {
-                menorPesoAresta[v] = peso;
-                verticePai[v] = u;
-                cout << "Atualizando aresta com menor peso do vértice " << v << " para " << peso << "\n";
-            }
-        }
-    }
-
-    cout << "\nÁrvore Geradora Mínima (Prim):\n";
-    int pesoTotal = 0;
-    for (int v = 1; v < numVertices; v++)
-    {
-        if (verticePai[v] != -1)
-        {
-            cout << "Aresta: (" << verticePai[v] << " - " << v << ") com peso " << menorPesoAresta[v] << "\n";
-            pesoTotal += menorPesoAresta[v];
-        }
-    }
-    cout << "\nPeso total da árvore: " << pesoTotal << "\n";
-}
-
 void dijkstra(int Origem, int Destino)
 {
     int origem = Origem;
@@ -445,10 +306,7 @@ void exibirMenu()
     cout << "8. Consultar Próximo Adjacente\n";
     cout << "9. Consultar Lista Completa de Adjacentes\n";
     cout << "10. Exportar Grafo\n";
-    cout << "11. Busca em profundidade\n";
-    cout << "12. Busca em largura\n";
-    cout << "13. Árvore geradora mínima (Prim)\n";
-    cout << "14. Menor caminho (Dijkstra)\n";
+    cout << "11. Menor caminho (Dijkstra)\n";
     cout << "0. Sair\n\n";
     cout << "Escolha: ";
 }
@@ -565,23 +423,6 @@ int main()
             break;
         }
         case 11:
-        {
-            cout << "Busca em profundidade:\n\n";
-            buscaEmProfundidade();
-            break;
-        }
-        case 12:
-        {
-            cout << "Busca em largura:\n\n";
-            buscaEmLargura();
-            break;
-        }
-        case 13:
-        {
-            prim();
-            break;
-        }
-        case 14:
         {
             int origem, destino;
             cout << "Digite o nome do vértice de origem: ";
